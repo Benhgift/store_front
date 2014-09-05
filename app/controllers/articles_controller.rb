cf72cf5066ac09424b2071e6486b0d744074a44c
Ben Gift <benhgift@gmail.com>
2014-09-05 14:51:41 -0700

    can now make new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,25 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    puts "============================-"
+    puts params
+    puts article_params
+    puts params[article_params]
+    puts @article.inspect
+
+    @article.save
+    redirect_to @article
   end
+
+  def show
+    @article = Article.find(params[:id])
+    puts "============================-"
+    puts @article.inspect
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
