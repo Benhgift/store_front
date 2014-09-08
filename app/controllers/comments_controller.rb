44e45596ddc4bda16356bd9c65407b56720737e7
Ben Gift <benhgift@gmail.com>
2014-09-08 14:02:38 -0700

    added basic auth

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,24 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "admin", password: "admin", only: :destroy
+
   def create
     get_article_by_id
-    @comment = @article.comments.create(comment_params)
+    make_comment_from_params
+    redirect_to article_path(@article)
+  end
+
+  def destroy 
+    get_article_by_id
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
     redirect_to article_path(@article)
   end
 
   private
+  def make_comment_from_params
+    @comment = @article.comments.create(comment_params)
+  end
+
   def get_article_by_id
     @article = Article.find(params[:article_id])
   end
